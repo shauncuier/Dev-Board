24d67e9b0b1a35504466d69cad7eb005520ecb25
Jashedul Islam Shaun <shauncuier@gmail.com>
2025-02-28 23:35:30 +0600

    refactor background functionality: move script to new location and enhance color generation

---
+++ function/function.js
@@ -0,0 +1,11 @@
+
+// Change the background color
+
+function randomBackground() {
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);    
+    let b = Math.floor(Math.random() * 255);
+    document.body.style.backgroundColor = `rgb(${r},${g},${b})`;
+}
+
+
--- js/bg.js
+++
@@ -1,6 +0,0 @@
-function randomBackground() {
-    let r = Math.floor(Math.random() * 256);
-    let g = Math.floor(Math.random() * 256);    
-    let b = Math.floor(Math.random() * 256);
-    document.body.style.backgroundColor = `rgb(${r},${g},${b})`;
-}
